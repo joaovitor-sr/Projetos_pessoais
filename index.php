6ce9a0d7638c59f766bec50a4dbf099b291c9e63
= <=>
2020-02-29 19:04:21 -0300

    atualizando o index

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="pt_br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index</title>
-</head>
-<body>
-     <h1>Site index</h1>
-</body>
-</html>
+ <html>
+  <head>
+   <meta charset="UTF-8">
+   <tilte>Index</title>
+   <link rel="stylesheet" href="edic/estilo.css">
+  </head>
+  <body>
+   <div class="botao_inical" id="botaoInicial">
+    <form method="post">
+     <a href="form_cadastrar"><button id="link" class="link">Cadastrar</button></a><br>
+     <a href="form-login"><button id="link" class="link">Logar</button></a><br>
+    </form>
+  </body>
+ </html> 
