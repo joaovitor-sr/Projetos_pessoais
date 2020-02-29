b3cd8b2ed1ea0894788a00dbf91b539c0a155ad4
= <=>
2020-02-29 19:36:44 -0300

    Layout

--- index.php
+++ index.php
@@ -2,14 +2,20 @@
  <html>
   <head>
    <meta charset="UTF-8">
-   <tilte>Index</title>
+   <title>Index</title>
    <link rel="stylesheet" href="edic/estilo.css">
+   <style>
+    body{
+        background-color: white;
+    }
+   </style>
   </head>
   <body>
-   <div class="botao_inical" id="botaoInicial">
     <form method="post">
+    <div class="botao_inical" id="botao_inicial">
      <a href="form_cadastrar"><button id="link" class="link">Cadastrar</button></a>
      <a href="form-login"><button id="link" class="link">Logar</button></a>
     </form>
+    </div>
   </body>
  </html> 
