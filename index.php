3c05dfc7b974f944553750c1b3836b62cf1f8746
= <=>
2020-02-29 19:12:58 -0300

    enviar dados novos

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
   <body>
    <div class="botao_inical" id="botaoInicial">
     <form method="post">
-     <a href="form_cadastrar"><button id="link" class="link">Cadastrar</button></a><br>
-     <a href="form-login"><button id="link" class="link">Logar</button></a><br>
+     <a href="form_cadastrar"><button id="link" class="link">Cadastrar</button></a>
+     <a href="form-login"><button id="link" class="link">Logar</button></a>
     </form>
   </body>
  </html> 
